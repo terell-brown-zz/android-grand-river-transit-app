ee6b13f9592643cc11bd69cfadfe6b62f16b33b7
Terell Brown <terellbrown.93@gmail.com>
2015-04-04 15:37:36 -0600

    Added additional folders to intial commit

---
+++ app/src/main/java/tbrown/com/woodbuffalotransitmockup/Activity_Main.java
@@ -0,0 +1,50 @@
+package tbrown.com.woodbuffalotransitmockup;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Activity_Main extends ActionBarActivity {
+
+
+    /* When using Appcombat support library you need to extend Main Activity to ActionBarActivity.
+     */
+
+    // Declaring the Toolbar Object
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity__main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
